15d68db7bdf4dba4fa27ffb5d13d66e4d0eeba39
akbar zidan <akbarzidan123@gmail.com>
2021-09-29 14:27:40 +0700

    Survival-Shooter

---
+++ Assets/Scripts/Player/PlayerDetector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public GameOverManager gameOverManager;
+ 
+    private void OnTriggerEnter(Collider other)
+    {   
+        // Debug.Log(other);
+        if (other.tag == "Enemy" && !other.isTrigger)
+        {
+            // Debug.Log("Warning");
+            float enemyDistance = Vector3.Distance(transform.position,other.transform.position);
+            gameOverManager.ShowWarning(enemyDistance);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Survival-Shooter
+
+Game ini bertujuan agar player bisa bertahan hidup selama mungkin dengan cara menembak musuh
+
+W > jalan ke atas
+
+A > Jalan Kekiri
+
+S > Jalan Kebawah
+
+D > Jalan ke kanan
+
+Left Klick > Shoot
